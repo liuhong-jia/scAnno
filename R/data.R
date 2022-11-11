ccd31928ac83f14de152f7781c3cd2f2d33e9df0
liuhong-jia <115637576+liuhong-jia@users.noreply.github.com>
2022-11-11 15:07:07 +0800

    Create data.R

---
+++ R/data.R
@@ -0,0 +1,10 @@
+#' @title Example Subset of Shen-Orr deconvolution data set.
+#' @description
+#' A subset of data from Shen-Orr et al. Triplicate samples of liver, brain and lung tissue were extracted from rats. RNA was extracted and mixed in known quantities. Gene expressions were measured using the Affymetrix Rat Genome 230 2.0 Array. True mixture proportions were known from experimental design. Gene expression measurements were summarized by RMA at the log2 level. Cell types reported are Liver, Brain and Lung. Data set introduced in 'Cell type-specific gene expression differences in complex tissues' by Shen-Orr et al. 
+#' @format List of lists. 
+#' \describe{
+#' \item{data}{list of data sets}
+#' \item{annotation}{annotation for the data set}
+#' }
+"GSE136103"
+"Human_cell_landscape"
