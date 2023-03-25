cf4697bb0563586f25aa792a74000a82ed8a2a72
liuhong-jia <115637576+liuhong-jia@users.noreply.github.com>
2023-03-25 15:21:32 +0800

    Update findMarkers.R

--- R/findMarkers.R
+++ R/findMarkers.R
@@ -10,7 +10,7 @@
 
 ##########################################################################################################
 
-searchMarkersByCorr <- function(ref.expr, query.genes, scale.data = TRUE, p.cut = 0.1, seed.num = 10, tcga.data.u = NULL) {
+searchMarkersByCorr <- function(ref.expr, query.genes, scale.data = TRUE, p.cut = 0.01, seed.num = 10, tcga.data.u = NULL) {
 	options (warn = -1)
 	if (!inherits(query.genes, 'list')) stop(sprintf('%s must be a list of query genes, exiting...', deparse(substitute(query.genes))))
 	if (scale.data) {
